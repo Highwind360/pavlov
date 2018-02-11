b7b1fc19febee77d94fbe1b63d502f6b57981124
Grayson Sinclair <highwind360@gmail.com>
2018-02-10 17:59:28 -0800

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .pavlov import main()
+
+main()
---
+++ settings.py
@@ -0,0 +1,9 @@
+# TODO: add the root as a variable
+
+DATABASE_URL = "sqlite://pavlov.db"
+
+COOKIE_COST = 10
+EXERCISES = {
+    "Push-Up": { "name": "push-ups", "value": 40, "goal": 120 },
+    "Run": { "name": "meters of running", "value": 100, "goal": 2000 }
+}
